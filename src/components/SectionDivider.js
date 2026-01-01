d38da24300d4f2ab161b778422aa8cacf50fb5ab
MahboobAlam0 <mahboobalam7131@gmail.com>
2026-01-01 13:17:13 +0530

    Refractor

--- src/components/SectionDivider.js
+++ src/components/SectionDivider.js
@@ -4,16 +4,7 @@ import '../styles/SectionDivider.css';
 const SectionDivider = () => {
     return (
         <div className="section-divider-container">
-            <div className="orbital-flux">
-                <div className="flux-beam"></div>
-                <div className="energy-strand strand-1"></div>
-                <div className="energy-strand strand-2"></div>
-                <div className="particles">
-                    {[...Array(5)].map((_, i) => (
-                        <div key={i} className="particle" style={{ '--i': i }}></div>
-                    ))}
-                </div>
-            </div>
+            <div className="section-divider-line"></div>
         </div>
     );
 };
